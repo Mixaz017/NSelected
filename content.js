514bad8e5b613eedffb9f808ae2af0ca205a900b
Mixaz <mixazplus@gmail.com>
2021-07-30 00:55:56 +0900

    Use exponent indicator where applicable

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 document.addEventListener('selectionchange', () => {
 	let selectedLength = document.getSelection().toString().length
-	chrome.runtime.sendMessage(selectedLength == 0 ? '' : selectedLength < 10000 ? selectedLength.toString() : '10k+')
+	chrome.runtime.sendMessage(selectedLength == 0 ? '' : selectedLength < 1e4 ? selectedLength.toString() : '10k+')
 })
